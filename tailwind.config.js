ef3f5f90f4c3805cdb8b669663e0159f06a43692
Behrouz Babaeinezhad <behruz.pb@gmail.com>
2023-10-04 18:27:02 +0200

    add ui library

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        fantasy: ["Fantasy", "cursive"],
+      },
       colors: {
         dark: {
           900: "#171717",
