7b146805f0b1f9572f8a0a57eef5d46ea293174d
Behrouz Babaeinezhad <behruz.pb@gmail.com>
2023-10-06 21:35:42 +0200

    improve tests

--- src/events/dashboard/create-edit-event/createEditEvent.test.tsx
+++ src/events/dashboard/create-edit-event/createEditEvent.test.tsx
@@ -1,69 +1,76 @@
 import { describe, it, expect } from "vitest";
-import {
-  fireEvent,
-  getByRole,
-  render,
-  screen,
-  waitFor,
-} from "@testing-library/react";
+import { cleanup, getByRole, render, screen } from "@testing-library/react";
 import { EventFormDialog } from "events/dashboard/create-edit-event/createEditEvent";
 import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
 import { AvailableEventTypeUrl } from "./consts";
-import UserEvent from "@testing-library/user-event";
+import { userEvent } from "@testing-library/user-event";
+import { requiredErrorMessage } from "./formSchema";
 
-const mock = new MockAdapter(axios);
+const eventsTypeMockData = {
+  CROSPROMO: "CROSPROMO",
+  LIVEOPS: "LIVEOPS",
+  APP: "APP",
+  ADS: "ADS",
+};
+
+function expectRequiredErrorMessages(expectedCount: number) {
+  const errorMessages = screen.queryAllByText(requiredErrorMessage, {
+    exact: false,
+  });
+  expect(errorMessages.length).toBe(expectedCount);
+}
+
+async function waitForValidation() {
+  await new Promise((resolve) => setTimeout(resolve, 100)); // Adjust the delay as needed
+}
 
 describe("Event Create/Edit Dialog", () => {
-  const errorMessage = "String must contain at least 1 character(s)";
-  const checkTextError = async (textField: HTMLElement) => {
-    fireEvent.change(textField, { target: { value: "a" } });
-    fireEvent.change(textField, { target: { value: "" } });
-    await waitFor(() =>
-      expect(screen.queryByText(errorMessage)).toBeInTheDocument()
-    );
-    fireEvent.change(textField, { target: { value: "a" } });
-    await waitFor(() => expect(screen.queryByText(errorMessage)).toBeNull());
-  };
+  describe("Form Validation", () => {
+    beforeEach(() => {
+      const mock = new MockAdapter(axios);
+      mock.onGet(AvailableEventTypeUrl).reply(200, eventsTypeMockData);
+      render(<EventFormDialog open handleClose={() => {}} />);
+    });
 
-  it("should show and remove description error message", async () => {
-    render(<EventFormDialog open handleClose={() => {}} />);
-    const eventDescription = screen
-      .getByTestId("event-description")
-      .querySelector("textarea");
-    checkTextError(eventDescription!);
-  });
+    afterEach(() => {
+      cleanup();
+    });
 
-  it("submit button should be disabled and enabled according to form status", async () => {
-    const eventsTypeMockData = {
-      CROSPROMO: "CROSPROMO",
-      LIVEOPS: "LIVEOPS",
-      APP: "APP",
-      ADS: "ADS",
-    };
+    it('displays "required" error messages', async () => {
+      expectRequiredErrorMessages(0);
+      const submitButton = screen.getByTestId("confirm-create-edit");
+      userEvent.click(submitButton);
+      await waitForValidation();
+      expectRequiredErrorMessages(3);
+    });
 
-    mock.onGet(AvailableEventTypeUrl).reply(200, eventsTypeMockData);
-    render(<EventFormDialog open handleClose={() => {}} />);
+    it('removes "required" error messages when input fields are filled', async () => {
+      const submitButton = screen.getByTestId("confirm-create-edit");
+      userEvent.click(submitButton);
+      await waitForValidation();
 
-    const nameTextField = screen
-      .getByTestId("event-name")
-      .querySelector("input");
-    const descriptionTextField = screen
-      .getByTestId("event-description")
-      .querySelector("textarea");
+      const nameTextField = screen
+        .getByTestId("event-name")
+        .querySelector("input");
 
-    const slider = screen.getByRole("slider");
-    UserEvent.click(getByRole(screen.getByTestId("event-type"), "button"));
-    await waitFor(() => UserEvent.click(screen.getByText(/LIVEOPS/i)));
-    fireEvent.input(nameTextField!, { target: { value: "a" } });
-    fireEvent.input(descriptionTextField!, { target: { value: "b" } });
-    console.log(descriptionTextField!.value);
-    fireEvent.input(slider, { target: { value: "2" } });
-    await new Promise((resolve) => setTimeout(resolve, 100));
-    const confirmBUtton = screen.getByLabelText("Confirm");
-    await waitFor(() => expect(confirmBUtton).toBeEnabled());
-    fireEvent.input(nameTextField!, { target: { value: "" } });
-    await new Promise((resolve) => setTimeout(resolve, 100));
-    await waitFor(() => expect(confirmBUtton).toBeDisabled());
+      const descriptionTextField = screen
+        .getByTestId("event-description")
+        .querySelector("textarea");
+
+      userEvent.type(descriptionTextField!, "a");
+      await waitForValidation();
+      expectRequiredErrorMessages(2);
+
+      userEvent.type(nameTextField!, "a");
+      await waitForValidation();
+      expectRequiredErrorMessages(1);
+
+      userEvent.click(getByRole(screen.getByTestId("event-type"), "button"));
+      await waitForValidation();
+      await userEvent.click(screen.getByText(/LIVEOPS/i));
+      await waitForValidation();
+      expectRequiredErrorMessages(0);
+    });
   });
 });
